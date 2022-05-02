de29e511186175a48595a957223f1a5d6735ffd1
MikkelS <mikkelfs@gmail.com>
2022-05-02 11:48:21 +0200

    List movies on client side

--- client/index.jsx
+++ client/index.jsx
@@ -1,23 +1,81 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
 import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 
 
 function FrontPage() {
-    return <div>
+    return (<div>
         <h1>Movie database</h1>
         <ul>
             <li><Link to={"/movies"}>List movies</Link></li>
             <li><Link to={"/movies/new"}>Add movie</Link></li>
         </ul>
-    </div>;
+    </div>
+    );
 }
 
+function useLoading(loadingFunction) {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+    const [data, setData] = useState();
+
+    return {loading, error, data};
+
+    async function load() {
+        try {
+            setLoading(true);
+            setData(await loadingFunction());
+        } catch (error) {
+            setError(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        load();
+    }, []);
+
+    return {loading, error, data};
+}
+
+async function fetchJSON(url) {
+    const res = await fetch(url);
+    if (!res.ok) {
+        throw new Error(`Failed to load ${res.status}: ${res.statusText}`);
+    }
+    return await res.json();
+}
+
+
 function ListMovies() {
+
+    const {loading, error, data} = useLoading( async () => {
+        await fetchJSON("/api/movies")
+    });
+
+    if(loading) {
+        return <div>Loading...</div>;
+    }
+    if (error) {
+        return (
+            <div>
+                <h1>Error</h1>
+                <div>{error.toString()}</div>
+            </div>
+        )
+    }
+
     return (
         <div>
             <h1>Movies in the database</h1>
+
+            <ul>
+                {data.map((movie) => {
+                    <li key={movie.title}>{movie.title}</li>
+                })}
+            </ul>
         </div>
     );
 }
@@ -27,7 +85,7 @@ function AddNewMovie() {
         <form>
             <h1>Add new movie</h1>
         </form>
-    )
+    );
 }
 
 function Application() {
